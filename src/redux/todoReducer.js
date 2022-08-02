087d2eee8212a48010e93641e0851f3c1571c564
RavitejaKambhampati23 <107537292+RavitejaKambhampati23@users.noreply.github.com>
2022-08-02 17:32:35 +0530

    useless comments removed

--- src/redux/todoReducer.js
+++ src/redux/todoReducer.js
@@ -5,24 +5,6 @@ const createTodoReducer = createSlice({
   name: "todos",
   initialState,
   reducers: {
-    // createTodo: (state, action) => {
-    //   state.push(action.payload);
-    //   return state;
-    // },
-    // deleteTodo: (state, action) => {
-    //   return state.filter((item) => item.id !== action.payload);
-    // },
-    // updateTodo: (state, action) => {
-    //   return state.map((todo) => {
-    //     if (todo.id === action.payload.id) {
-    //       return {
-    //         ...todo,
-    //         item: action.payload.item,
-    //       };
-    //     }
-    //     return todo;
-    //   });
-    // },
     updateTodoList: (state, action) => {
       return { tasks: action.payload };
     },
